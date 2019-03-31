c0304096d246deb50f99c6d5aa9e5389c89c2d5c
tzuyichiu <47012056+tzuyichiu@users.noreply.github.com>
2019-04-01 01:36:28 +0200

    Add files via upload

--- turtlebot3_goal.py
+++ turtlebot3_goal.py
@@ -8,6 +8,13 @@ from geometry_msgs.msg import Point
 from geometry_msgs.msg import Twist
 from math import atan2
 
+BURGER_MAX_LIN_VEL = 0.22
+BURGER_MAX_ANG_VEL = 2.84
+
+err = """
+Communications Failed or Error Compilation
+"""
+
 def newOdom(msg):
 	global x, y, theta
 	x = msg.pose.pose.position.x
@@ -15,13 +22,6 @@ def newOdom(msg):
 
 	rot_q = msg.pose.pose.orientation
 	theta = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])[2]
-
-BURGER_MAX_LIN_VEL = 0.22
-BURGER_MAX_ANG_VEL = 2.84
-
-err = """
-Communications Failed or Error Compilation
-"""
 
 def info(pos_x, pos_y, inc_x, inc_y, inc_theta):
     return "Pos: (%.3f, %.3f)\tMove: (%.3f, %.3f)\tTurn: %3.1f" % (pos_x, pos_y, inc_x, inc_y, inc_theta*180/3.1415)
@@ -42,6 +42,9 @@ def checkLinearLimitVelocity(vel):
 def checkAngularLimitVelocity(vel):
     vel = constrain(vel, -BURGER_MAX_ANG_VEL, BURGER_MAX_ANG_VEL)
     return vel
+
+LIN_VEL = checkLinearLimitVelocity(0.2)
+ANG_VEL = checkAngularLimitVelocity(0.5)
 
 x = 0.0
 y = 0.0
@@ -69,6 +72,10 @@ while not rospy.is_shutdown():
 	inc_x = goal.x - x
 	inc_y = goal.y - y
 	inc_theta = atan2(inc_y, inc_x) - theta
+	if inc_theta > 3.14:
+		inc_theta -= 6.28
+	if inc_theta < -3.14:
+		inc_theta += 6.28
 
 	if abs(inc_x) < 0.01 and abs(inc_y) < 0.01:
 		twist = Twist()
@@ -76,7 +83,7 @@ while not rospy.is_shutdown():
 		twist.angular.z = 0.0
 		pub.publish(twist)
 		print info(x, y, inc_x, inc_y, inc_theta)
-		print "\nYour robot has arrived at it's destination (%.2f, %.2f) with error of (%.3f, %.3f)" % (goal.x, goal.y, inc_x, inc_y)
+		print "\nYour robot has arrived at its destination (%.2f, %.2f) with error of (%.3f, %.3f)" % (goal.x, goal.y, inc_x, inc_y)
 		sys.exit()
 
 	print info(x, y, inc_x, inc_y, inc_theta)
@@ -84,11 +91,11 @@ while not rospy.is_shutdown():
 	if abs(inc_theta) > 0.1:
 		twist.linear.x = 0.0
 		if inc_theta < 0:
-			twist.angular.z = -0.5
+			twist.angular.z = -ANG_VEL
 		else:
-			twist.angular.z = 0.5
+			twist.angular.z = ANG_VEL
 	else:
-		twist.linear.x = 0.2
+		twist.linear.x = LIN_VEL
 		twist.angular.z = 0.0
 
 	pub.publish(twist)
